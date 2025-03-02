aae84cf10c110debc2dc92af86c09ddd6dee4772
Abimbola <Haryishar2@gmail.com>
2025-03-02 12:24:44 +0100

    tasks

--- test/test_student.py
+++ test/test_student.py
@@ -2,8 +2,9 @@ import unittest
 
 
 class MyStudentTestCase(unittest.TestCase):
-    def test_so(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_that_student_can_register_and_in_with_email_password_and_name(self):
+        student = Student("abimbolaabishat@gmail.com", "password", "favour Igwe")
+
 
 
 
